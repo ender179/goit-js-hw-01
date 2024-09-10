2afdba02384dee4d66f9693f51e8499595c092ba
ender179 <kirilukvlad91@gmail.com>
2024-09-10 22:37:33 +0300

    Update index.html

--- js/task-3.js
+++ js/task-3.js
@@ -1,34 +1,28 @@
-function calcAverageCalories(days) {
-if (days.length === 0) return 0;
+const profile = {
+username: "Jacob",
+playTime: 300,
   
-const totalCalories = days.reduce((sum, day) => sum + day.calories, 0);
-return totalCalories / days.length;
+// Метод для зміни імені профілю
+changeUsername(newName) {
+this.username = newName;
+},
+  
+// Метод для збільшення ігрових годин
+updatePlayTime(hours) {
+this.playTime += hours;
+},
+  
+// Метод для отримання інформації про профіль
+getInfo() {
+return `${this.username} has ${this.playTime} active hours!`;
 }
+};
   
-console.log(
-calcAverageCalories([
-{ day: "monday", calories: 3010 },
-{ day: "tuesday", calories: 3200 },
-{ day: "wednesday", calories: 3120 },
-{ day: "thursday", calories: 2900 },
-{ day: "friday", calories: 3450 },
-{ day: "saturday", calories: 3280 },
-{ day: "sunday", calories: 3300 }
-])
-); // 3180
+// Код для перевірки
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
   
-console.log(
-calcAverageCalories([
-{ day: "monday", calories: 2040 },
-{ day: "tuesday", calories: 2270 },
-{ day: "wednesday", calories: 2420 },
-{ day: "thursday", calories: 1900 },
-{ day: "friday", calories: 2370 },
-{ day: "saturday", calories: 2280 },
-{ day: "sunday", calories: 2610 }
-])
-); // 2270
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
   
-console.log(
-calcAverageCalories([])
-); // 0
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
